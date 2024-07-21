ae938178de9bd09c15638cc82de8cad87f6169d2
63BeetleSmurf <scott@murphys.place>
2024-07-21 07:52:28 +0100

    Reimplement split on selected text.

--- Pinny Notes/Tools/SplitTool.cs
+++ Pinny Notes/Tools/SplitTool.cs
@@ -8,6 +8,8 @@ namespace Pinny_Notes.Tools;
 
 public partial class SplitTool : BaseTool, ITool
 {
+    private string? _selectedText = null;
+
     public enum ToolActions
     {
         SplitComma,
@@ -22,13 +24,23 @@ public partial class SplitTool : BaseTool, ITool
         _menuActions.Add(new("Comma", MenuActionCommand, ToolActions.SplitComma));
         _menuActions.Add(new("Space", MenuActionCommand, ToolActions.SplitSpace));
         _menuActions.Add(new("Tab", MenuActionCommand, ToolActions.SplitTab));
-        //_menuActions.Add(new("-"));
-        //_menuActions.Add(new("Selected", MenuActionCommand, ToolActions.SplitSelected));
+        _menuActions.Add(new("-"));
+        _menuActions.Add(new("Selected", MenuActionCommand, ToolActions.SplitSelected));
     }
 
     [RelayCommand]
     private void MenuAction(ToolActions action)
     {
+        if (action != ToolActions.SplitSelected)
+        {
+            _selectedText = null;
+        }
+        else
+        {
+            _selectedText = _noteTextBox.SelectedText;
+            _noteTextBox.SelectionLength = 0;
+        }
+
         ApplyFunctionToNoteText(ModifyTextCallback, action);
     }
 
@@ -42,8 +54,10 @@ public partial class SplitTool : BaseTool, ITool
                 return text.Replace(" ", Environment.NewLine);
             case ToolActions.SplitTab:
                 return text.Replace("\t", Environment.NewLine);
-                //case ToolActions.SplitSelected:
-                //    return text.Replace(splitString, Environment.NewLine);
+            case ToolActions.SplitSelected:
+                if (!string.IsNullOrEmpty(_selectedText))
+                    return text.Replace(_selectedText, Environment.NewLine);
+                break;
         }
 
         return text;
