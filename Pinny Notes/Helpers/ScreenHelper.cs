3475afe20cf617930f6a77596b2216d06e02696b
63BeetleSmurf <scott@murphys.place>
2024-10-22 10:04:35 +0100

    Fix note positioning when using multiple monitors.

---
+++ Pinny Notes/Helpers/ScreenHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+
+namespace Pinny_Notes.Helpers;
+
+public static class ScreenHelper
+{
+    public static nint GetWindowHandle(Window window)
+    {
+        return new WindowInteropHelper(window).Handle;
+    }
+
+    public static Rectangle GetCurrentScreenBounds(nint windowHandle)
+    {
+        Screen currentScreen = Screen.FromHandle(windowHandle);
+        return currentScreen.Bounds;
+    }
+
+}
