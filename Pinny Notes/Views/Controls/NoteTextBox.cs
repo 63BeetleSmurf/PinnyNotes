c932997040075f509a491d6962094d52fc1d0a0e
63BeetleSmurf <scott@murphys.place>
2024-11-21 17:13:35 +0000

    Add custom text box control for notes.

---
+++ Pinny Notes/Views/Controls/NoteTextBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PinnyNotes.WpfUi.Views.Controls;
+
+public class NoteTextBox : TextBox
+{
+    public NoteTextBox()
+    {
+    }
+}
