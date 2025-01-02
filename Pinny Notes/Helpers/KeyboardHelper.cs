25b22abe2d0ae9ea1171b7e38668546577aeae6d
63BeetleSmurf <scott@murphys.place>
2025-01-02 09:28:10 +0000

    Allow clearing selection in note view.

---
+++ Pinny Notes/Helpers/KeyboardHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Input;
+
+namespace PinnyNotes.WpfUi.Helpers;
+
+public static class KeyboardHelper
+{
+    public static bool IsShiftPressed()
+        => ((Keyboard.Modifiers & ModifierKeys.Shift) == ModifierKeys.Shift);
+
+    public static bool IsControlPressed()
+        => ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control);
+}
--- Pinny Notes/Views/ManagementWindow.xaml.cs
+++ Pinny Notes/Views/ManagementWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
 
+using PinnyNotes.WpfUi.Helpers;
 using PinnyNotes.WpfUi.Models;
 using PinnyNotes.WpfUi.Presenters;
 using PinnyNotes.WpfUi.Services;
@@ -16,10 +19,23 @@ public partial class ManagementWindow : Window
         InitializeComponent();
 
         _presenter = new(managementService, model, this);
+
+        NotesListView.MouseUp += OnListViewMouseUp;
     }
 
     public void DisplayNotes(IEnumerable<NoteModel> notes)
     {
         NotesListView.ItemsSource = notes;
+    }
+
+    private void OnListViewMouseUp(object? sender, MouseButtonEventArgs e)
+    {
+        // Hit testing to determine where the right-click occurred
+        var hitTestResult = VisualTreeHelper.HitTest(NotesListView, e.GetPosition(NotesListView));
+        var item = hitTestResult?.VisualHit as FrameworkElement;
+        var listViewItem = item?.DataContext as NoteModel;
+
+        if (listViewItem == null && !KeyboardHelper.IsShiftPressed() && !KeyboardHelper.IsControlPressed())
+            NotesListView.UnselectAll();
     }
 }
